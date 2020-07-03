ef6f2bccff39ba30d0571fee9736a5f04b4f1ae4
David Négrier <d.negrier@thecodingmachine.com>
2020-07-03 18:41:26 +0200

    Fixing imports

--- src/action.js
+++ src/action.js
@@ -1,7 +1,7 @@
 const glob = require("glob");
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { isJsonTiledMap, processTileset } = require('./tiledOptimizer');
+const { isJsonTiledMap, processTileset } = require('map-optimizer/src/tiledOptimizer');
 
 try {
     let mapFiles = [];
