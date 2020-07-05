b82c9deef4ed64d71ce65b251a3c3b1593cf6ac3
David Négrier <d.negrier@thecodingmachine.com>
2020-07-05 11:15:54 +0200

    Fixing issue with several maps

--- src/action.js
+++ src/action.js
@@ -4,23 +4,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { isJsonTiledMap, optimizeMap } = require('map-optimizer');
 
-try {
-    let mapFiles = [];
-    const mapFile = core.getInput('map-file');
-    if (mapFile) {
-        mapFiles.push(mapFile);
-    } else {
-        // Let's scan the system for maps.
-        mapFiles = glob.sync('**/*.json');
-        mapFiles = mapFiles.filter(isJsonTiledMap);
-    }
+(async function() {
+    try {
+        let mapFiles = [];
+        const mapFile = core.getInput('map-file');
+        if (mapFile) {
+            mapFiles.push(mapFile);
+        } else {
+            // Let's scan the system for maps.
+            mapFiles = glob.sync('**/*.json');
+            mapFiles = mapFiles.filter(isJsonTiledMap);
+        }
 
-    for (let mapFile of mapFiles) {
-        let mapDir = path.dirname(mapFile);
-        let outputDir = path.resolve(process.env.GITHUB_WORKSPACE ? process.env.GITHUB_WORKSPACE : '.', mapDir);
-        optimizeMap(mapFile, outputDir, 1, 0xffffff00);
+        for (let mapFile of mapFiles) {
+            let mapDir = path.dirname(mapFile);
+            let outputDir = path.resolve(process.env.GITHUB_WORKSPACE ? process.env.GITHUB_WORKSPACE : '.', mapDir);
+            await optimizeMap(mapFile, outputDir, 1, 0xffffff00);
+        }
+    } catch (error) {
+        console.error(error);
+        core.setFailed(error.message);
     }
-} catch (error) {
-    console.error(error);
-    core.setFailed(error.message);
-}
+}());
+
